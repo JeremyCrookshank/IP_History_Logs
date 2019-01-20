b80ce234bc6bb7ad38bd665d734ef5c3a4aacb41
JeremyCrookshank <46864828+JeremyCrookshank@users.noreply.github.com>
2019-01-20 12:49:05 +0000

    Add files via upload

---
+++ IP History/admin/modules/tools/ip_history_logs.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * MyBB 1.8
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+$page->add_breadcrumb_item("IP History", "index.php?module=tools-ip_history_logs");
+$sub_tabs['ip_history_logs'] = array(
+	'title' => 'IP History Logs',
+	'link' => "index.php?module=tools-ip_history_logs",
+	'description' => ''
+);
+
+$plugins->run_hooks("admin_tools_ip_history_logs_begin");
+$page->output_header('IP History Logs');
+$plugins->run_hooks("admin_tools_ip_history_logs_view");
+
+?>
+
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/zf-6.4.3/jq-3.3.1/dt-1.10.18/datatables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/v/zf-6.4.3/jq-3.3.1/dt-1.10.18/datatables.min.js"></script>
+
+
+<script>
+$(document).ready(function() {
+    $('.HistoryTable').DataTable();
+    $('.HistoryTable').removeClass("dataTable");
+} );
+</script>
+
+ <style>
+ #inner {
+ 	display:flex;
+ }
+.HistoryTable {
+ margin: 0, 0 !important; 
+}
+
+.menu .active>a {
+    background: inherit !important;
+}
+
+</style>
+	<?php
+
+	// TODO
+	// Query Max for large forums & Query Filters
+	$query = $db->simple_select("ip_history", "*", "", array('order_by' => "date"));
+	$table = new Table();
+	$table->construct_header("UserID", array('class' => "align_center")); 
+	$table->construct_header("Page");
+	$table->construct_header("Useragent",  array('class' => "align_center"));
+	$table->construct_header("IP",  array('class' => "align_center"));
+	$table->construct_header("Date",  array('class' => "align_center"));
+	// Output Every row from DB
+while($section = $db->fetch_array($query))
+	{
+		$table->construct_cell("<a href=\"/user-{$section['uid']}.html\">{$section['uid']}</a>", array("class" => "align_center", "width" => '60'));
+		$table->construct_cell("<strong><a href=\"/{$section['page']}\">{$section['page']}</a></strong>");
+		$table->construct_cell($section['useragent']);
+		$table->construct_cell($section['ip'], array("class" => "align_center", "width" => '90'));
+		$table->construct_cell($section['date'], array("class" => "align_center", "width" => '90'));
+
+		$table->construct_row();
+	}
+
+	// If we have no data yet.
+	if($table->num_rows()  == 0)
+	{
+		$table->construct_cell("No Data", array('colspan' => 4));
+		$table->construct_row();
+	}
+
+	// Allows me to attach a class so I can have DataTables
+	$table->output("IP History Logs", 1, "HistoryTable", false);
+	?>
+</div>
+</div>
+	<?php
+
---
+++ IP History/inc/plugins/ip_history_logs.php
@@ -0,0 +1,230 @@
+<?php
+
+// Disallow direct access to this file for security reasons
+// TODO: Language File
+
+if (!defined("IN_MYBB")) {
+	die("Direct initialization of this file is not allowed.");
+}
+
+$plugins->add_hook('global_start', 'ip_history_logs_record_ip');
+$plugins->add_hook("admin_tools_menu_logs", "ip_history_logs_admin_menu");
+$plugins->add_hook("admin_tools_action_handler", "ip_history_logs_admin_action_handler");
+$plugins->add_hook("admin_tools_permissions", "ip_history_logs_admin_permissions");
+
+function ip_history_logs_info()
+{
+	return array(
+		"name" => "IP History",
+		"description" => "This keeps a record of a users IP history as they use the website. This is useful for auditing fraud/ban evaders/general/when people start using VPNS/Proxys during their user activity. It can record every instance of a users IP when it changes and how often, the page they were viewing and their useragent.",
+		"website" => "",
+		"author" => "Jeremy Crookshank",
+		"authorsite" => "",
+		"version" => "1.0",
+		"guid" => "",
+		"compatibility" => "*"
+	);
+}
+
+function ip_history_logs_admin_menu($sub_menu)
+{
+	// We can add ours to the bottom of logs list
+	$key = count($sub_menu) + 1; 
+    $sub_menu[$key] = array('id' => 'ip_history_logs', 'title' => 'IP History Logs', 'link' => 'index.php?module=tools-ip_history_logs');
+    return $sub_menu;
+}
+
+function ip_history_logs_admin_action_handler($actions)
+{
+    $actions['ip_history_logs'] = array('active' => 'ip_history_logs', 'file' => 'ip_history_logs.php');
+    return $actions;
+}
+
+// Add Permissions to view
+function ip_history_logs_admin_permissions($admin_permissions)
+{
+    $admin_permissions['ip_history_logs'] = "Ability to view Users IP History";
+    return $admin_permissions;
+}
+
+function ip_history_logs_record_ip()
+{
+
+	// Restore Visitors IP if using CloudFlare
+	if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+  	$_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+	}
+
+	//Get basic session infomation
+	global $db, $mybb;
+	$ip = filter_input(INPUT_SERVER, 'REMOTE_ADDR');
+	$user = $mybb->user['uid'];
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$page = basename($_SERVER['REQUEST_URI']);
+
+	//Make sure they're logged in
+	if ($user != 0) {
+		if ($mybb->settings['enable_uniqueIP'] == 1) {
+			$ip_event = array(
+				"ip" => $ip,
+				"uid" => $user
+			);
+
+			// Optionals
+
+			if ($mybb->settings['enable_useragentrecord'] == 1) {
+				$ip_event["useragent"] = $db->escape_string($useragent);
+			}
+
+			if ($mybb->settings['enable_pagerecording'] == 1) {
+				$ip_event["page"] = $db->escape_string($page);
+			}
+
+			// Check the user hasn't had this IP before
+
+			$query = $db->simple_select("ip_history", "COUNT(*) as 'unique'", "IP='$ip'", array());
+			$unique = $db->fetch_field($query, "unique");
+			if ($unique == 0) {
+				$db->insert_query("ip_history", $ip_event);
+			}
+			else {
+				return;
+			}
+
+			// Else we want to track all user activity
+
+		}
+		else {
+			$ip_event = array(
+				"ip" => $ip,
+				"uid" => $user
+			);
+
+			// Optionals
+
+			if ($mybb->settings['enable_useragentrecord'] == 1) {
+				$ip_event["useragent"] = $db->escape_string($useragent);
+			}
+
+			if ($mybb->settings['enable_pagerecording'] == 1) {
+				$ip_event["page"] = $page;
+			}
+
+			$db->insert_query("ip_history", $ip_event);
+		}
+	}
+}
+
+function ip_history_logs_install()
+{
+	global $db, $mybb;
+	// Optimised for storing and retrieving IPV4 & IPV6 in an efficent manner
+	$db->write_query("CREATE TABLE IF NOT EXISTS `".TABLE_PREFIX."ip_history` (
+      `id` int(10) unsigned NOT NULL auto_increment,
+      `uid` int(10) NOT NULL,
+      `ip` VARBINARY(16) NOT NULL,
+      `page` VARCHAR(512) NULL,
+	  `useragent` varchar(255) NULL,
+	  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY  (`id`)
+    ) ENGINE=MyISAM  
+      COLLATE=utf8_general_ci
+	  DEFAULT CHARSET=utf8;
+     ");
+	 
+	 // Allow us to search for existing IP addresses faster
+
+	$db->write_query("CREATE INDEX IDX_IP ON " . TABLE_PREFIX . "ip_history (ip);");
+
+
+	$setting_group = array(
+		'name' => 'ip_history_logs',
+		'title' => 'IP History Settings',
+		'description' => 'Adjust the IP History settings',
+		'disporder' => 5, // The order your setting group will display
+		'isdefault' => 0
+	);
+	$gid = $db->insert_query("settinggroups", $setting_group);
+	$setting_array = array(
+		'enable_pagerecording' => array(
+			'title' => 'Enable Page Recording',
+			'description' => 'Do you want to record the page they visit?',
+			'optionscode' => 'yesno',
+			'value' => 1,
+			'disporder' => 1
+		) ,
+		'enable_uniqueIP' => array(
+			'title' => 'Record Unique IP',
+			'description' => 'Record only unique IPs for users. Unticking this may cause a large amount of data due to every time they view a page.',
+			'optionscode' => 'yesno',
+			'value' => 1,
+			'disporder' => 2
+		) ,
+		'enable_useragentrecord' => array(
+			'title' => 'Record User Agent',
+			'description' => 'Record the user agent for each record.',
+			'optionscode' => 'yesno',
+			'value' => 1,
+			'disporder' => 3
+		) ,
+		'ip_history_rem_date' => array(
+			'title' => 'Log Removal Days',
+			'description' => 'How often should logs be cleared? - 0 For never.',
+			'optionscode' => 'numeric',
+			'value' => 365,
+			'disporder' => 4
+		) ,
+	);
+	foreach($setting_array as $name => $setting) {
+		$setting['name'] = $name;
+		$setting['gid'] = $gid;
+		$db->insert_query('settings', $setting);
+	}
+
+	rebuild_settings();
+}
+
+function ip_history_logs_is_installed()
+{
+	global $db;
+	if ($db->table_exists("ip_history")) {
+		return true;
+	}
+
+	return false;
+}
+
+function ip_history_logs_uninstall()
+{
+	global $db;
+	$db->write_query("DROP TABLE " . TABLE_PREFIX . "ip_history");
+	$db->write_query("DROP INDEX IDX_IP ON " . TABLE_PREFIX . "ip_history");
+	$db->delete_query('settings', "name IN ('enable_pagerecording','enable_uniqueIP','enable_useragentrecord')");
+	$db->delete_query('settinggroups', "name = 'ip_history_logs'");
+	rebuild_settings();
+}
+function ip_history_logs_activate()
+{
+	global $db;
+	require_once MYBB_ROOT."inc/functions_task.php";
+	$new_task = array(
+		"title"			=> "IP History Log Removal",
+		"description"	=> "Automatically remove IP history logs based on days elapsed. Configured in settings.",
+		"file"			=> "ip_history",
+		"minute"		=> "0",
+		"hour"			=> "0",
+		"day"			=> "*",
+		"month"			=> "*",
+		"weekday"		=> "*",
+		"enabled"		=> 1,
+		"logging"		=> 1,
+		"locked"		=> 0
+	);
+
+	$new_task['nextrun'] = fetch_next_run($new_task);
+	$db->insert_query("tasks", $new_task);
+}
+
+function ip_history_logs_deactivate()
+{
+}
